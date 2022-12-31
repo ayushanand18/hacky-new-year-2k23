907a969e1dc698259429084656dd3eea14e16189
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2022-12-31 10:30:02 +0000

    feat: added chatbot

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,4 +6,8 @@ router.get("/", function(req, res) {
   res.render("home/index");
 });
 
+router.get("/chat", function(req, res) {
+  res.render("chat/index");
+});
+
 module.exports = router;
--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ dotenv.config({ path: ".env" });
 
 module.exports = {
   port: process.env.PORT || 5000,
-  env: process.env.NODE_ENV || "development",
-  secretKey: process.env.SECRET_KEY || "octocat",
-  githubClientId: process.env.GITHUB_CLIENT_ID || "",
-  githubClientSecret: process.env.GITHUB_CLIENT_SECRET || ""
+  env: process.env.NODE_ENV || "development"
+  // secretKey: process.env.SECRET_KEY || "octocat",
+  // githubClientId: process.env.GITHUB_CLIENT_ID || "",
+  // githubClientSecret: process.env.GITHUB_CLIENT_SECRET || ""
 };
