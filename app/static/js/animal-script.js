301ee73b825380e13ab28eabbb1bef9251eacad0
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2023-01-01 08:06:12 +0000

    added animal info

---
+++ app/static/js/animal-script.js
@@ -0,0 +1,61 @@
+// we will need a list of animals which we are going to include
+// lets keep 6 animals
+
+const animals = {
+    "3": "Pybmy Rabbit",
+    "4": "Red Kangaroo",
+    "5": "Koala Bear",
+    "8": "Elephant Shrew",
+    "9": "Siphonophores",
+    "10": "Pink Sea-Through Fantasia",
+}
+const animal_characteristics = {
+    "3":{
+        "name": "Pybmy Rabbit",
+        "photo": "https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Magazines/pygmy-rabbit-Betsy-Demay-570x375.ashx",
+        "fun_fact": "something fun about it",
+        "threats": "serious threats due to climate change",
+        "conserve": "you should do this",
+        "cta": "know more here"
+    },
+    "4":{
+        "name": "Red Kangaroo",
+        "photo": "https://cdn.shortpixel.ai/spai/q_lossy+ret_img+to_auto/https://cosmosmagazine.com/wp-content/uploads/2022/08/GettyImages-520278958-red-kangaroo.-Credit-Jami-Tarris.jpg",
+        "fun_fact": "something fun about it",
+        "threats": "serious threats due to climate change",
+        "conserve": "you should do this",
+        "cta": "know more here"
+    },
+    "5":{
+        "name": "Koala Bear",
+        "photo": "https://cdn.britannica.com/23/190523-050-112C739F/Koala-joey-back.jpg",
+        "fun_fact": "something fun about it",
+        "threats": "serious threats due to climate change",
+        "conserve": "you should do this",
+        "cta": "know more here"
+    },
+    "8":{
+        "name": "Elephant Shrew",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Rhynchocyon_petersi_from_side.jpg/1200px-Rhynchocyon_petersi_from_side.jpg",
+        "fun_fact": "something fun about it",
+        "threats": "serious threats due to climate change",
+        "conserve": "you should do this",
+        "cta": "know more here"
+    },
+    "9":{
+        "name": "Siphonophores",
+        "photo": "https://schmidtocean.org/wp-content/uploads/49250771427_9a6b0933e4_o.jpg",
+        "fun_fact": "something fun about it",
+        "threats": "serious threats due to climate change",
+        "conserve": "you should do this",
+        "cta": "know more here"
+    },
+    "10":{
+        "name": "Pink Sea-Through Fantasia",
+        "photo": "https://i.pinimg.com/originals/34/64/ed/3464edfc56322a45c8e7aa60e4021149.jpg",
+        "fun_fact": "something fun about it",
+        "threats": "serious threats due to climate change",
+        "conserve": "you should do this",
+        "cta": "know more here"
+    }
+}
